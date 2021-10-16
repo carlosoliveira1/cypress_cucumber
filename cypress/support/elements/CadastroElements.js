8e3f968e4c36e9624d1f3463d5bf429c3707ac71
carlosoliveira1 <carlosoliveiramauricio@gmail.com>
2021-10-16 00:30:46 -0300

    ajustando elementos do cadastro

--- cypress/support/elements/CadastroElements.js
+++ cypress/support/elements/CadastroElements.js
@@ -4,61 +4,45 @@ class CadastroElements {
 
     emailCad = () => { return '#email_create' }
 
-    botaoCreateAcount = () => {'#SubmitCreate > span'}
+    botaoCreateAcount = () => { '#SubmitCreate > span' }
 
     radioGender = () => { return '#id_gender1' }
-    //cy.get('#id_gender1').click();
-    fieldName = () => {return '#customer_firstname'}
-    //cy.get('#customer_firstname').type(faker.name.firstName());
-    fieldLastName = () => {return '#customer_lastname'}
-    //cy.get('#customer_lastname').type(faker.name.lastName());
 
-     fieldPassw = () => { return '#passwd' }
-    //cy.get('#passwd').type(faker.internet.password());
+    fieldName = () => { return '#customer_firstname' }
+
+    fieldLastName = () => { return '#customer_lastname' }
+
+    fieldPassw = () => { return '#passwd' }
 
     selectDays = () => { return '#days' }
-    //cy.get('#days').select('5').should('have.value', '5')
-    
+
     selectMonths = () => { return '#months' }
-    //cy.get().select('7').should('have.value', '7')
 
 
-      selectYears  = () => { return '#years' }
-    //cy.get('#years').select('1980').should('have.value', '1980')
+    years = () => { return '#years' }
 
     optin = () => { return '#optin' }
-    //cy.get('#optin').click()
 
     companyName = () => { return '#company' }
-    //cy.get('#company').type(faker.company.companyName())
 
-     adress1 = () => { return '#address1' }
-    //cy.get('#address1').type(faker.address.streetAddress())
+    adress1 = () => { return '#address1' }
 
     adress2 = () => { return '#address2' }
-    //cy.get('#address2').type(faker.address.secondaryAddress())
 
     cityName = () => { return '#city' }
-    //cy.get('#city').type(faker.address.city())
 
-    idState = () => {'#id_state'}
-    //cy.get('#id_state').select('Oklahoma').should('have.value', '36')
+    idState = () => { '#id_state' }
 
-    postcode = () => {return '#postcode'}
-    //cy.get('#postcode').type(faker.address.zipCodeByState())
+    postcode = () => { return '#postcode' }
 
-    other = () => {return '#other' }
-    //cy.get('#other').type(faker.lorem.paragraph())
+    other = () => { return '#other' }
 
-    phone = () => {return '#phone' }
-    //cy.get('#phone').type(faker.phone.phoneNumber())
+    phone = () => { return '#phone' }
 
-    phone_mobile = () => {return '#phone_mobile' }
-    //cy.get('#phone_mobile').type(faker.phone.phoneNumberFormat())
+    phone_mobile = () => { return '#phone_mobile' }
 
-    alias = () => {return '#alias'} 
-    //cy.get('#alias').clear()// Foi a forma que encontrei para a apafar o texto que estava no campo
-    //cy.get('#alias').type(faker.internet.email())
+    alias = () => { return '#alias' }
+
 
 
 
